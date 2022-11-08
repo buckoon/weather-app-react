cf9466c8b62f4c8b58d680984c05b6ce42feccda
buckoon <jeremy8@vt.edu>
2022-11-08 11:59:57 -0500

    Work in progress

--- src/App.js
+++ src/App.js
@@ -3,35 +3,60 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
+  const [data, setData] = React.useState({})
+  const [location, setLocation] = React.useState('')
 
-  /*const url='https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=137f22e6047c3b6fec57becb0acd9e87
-'
-  */return (
-    <div className="app">
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
       
+  }
+}
+
+  
+    return (
+    <div className="app">
+      <div className="search">
+        <input 
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter location'
+        type="text"/>
+      </div>
     <div className= "container">
       <div className= "top"></div>
       <div className="location">
-        <p>Dallas</p>
+        <p>{data.name}</p>
       </div>
       <div className="temp">
-        <h1>60</h1>
+        <h1>data.name.temp</h1>
       </div>
       <div className="description">
         <p>clouds</p>
       </div>
       <div className= "bottom">
         <div className="feels">
-          <p>65</p>
+          <p className="bold">65</p>
+          
+          <p>Feels Like</p>
         </div>
         <div className="humidity">
-          <p>45%</p>
+          <p className="bold">45%</p>
+          <p>Humidity</p>
         </div>
         <div className="wind">
-          <p>5 mph</p>
+          <p className="bold">5 mph</p>
+          <p>Wind Speed</p>
         </div>
       </div>
-
+      
     </div>
  
     </div>
