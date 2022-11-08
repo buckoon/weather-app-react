4d40db0317d10a4ca60ee476e2f7bbbc21d45c02
buckoon <jeremy8@vt.edu>
2022-11-07 20:41:26 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  /*const url='https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=137f22e6047c3b6fec57becb0acd9e87
+'
+  */return (
+    <div className="app">
+      
+    <div className= "container">
+      <div className= "top"></div>
+      <div className="location">
+        <p>Dallas</p>
+      </div>
+      <div className="temp">
+        <h1>60</h1>
+      </div>
+      <div className="description">
+        <p>clouds</p>
+      </div>
+      <div className= "bottom">
+        <div className="feels">
+          <p>65</p>
+        </div>
+        <div className="humidity">
+          <p>45%</p>
+        </div>
+        <div className="wind">
+          <p>5 mph</p>
+        </div>
+      </div>
+
+    </div>
+ 
     </div>
   );
 }
